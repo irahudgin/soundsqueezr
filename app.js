dc77ffecc807e7381b47309f36829912919c782f
Ira Hudgin <irahudgin@gmail.com>
2021-12-11 12:07:24 -0500

    trying to get the file to work

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { stat } = require("fs");
+const fs = require("fs");
 const app = express();
 const path = require("path");
 
 // If more than one html page staticPath has to change
 var staticPath = path.join(__dirname, "/public");
 
+app.use((req, res, next) => {
+  res.set("Cross-Origin-Embedder-Policy", "require-corp");
+  res.set("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(express.static(staticPath));
+app.use("/scripts", express.static(__dirname + "/node_modules/@ffmpeg/"));
 
 app.get("/", (req, res) => {
   res.sendFile(staticPath);
