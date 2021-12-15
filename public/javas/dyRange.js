fea53e5581a176e438863ae780706ae261ea8275
Ira Hudgin <irahudgin@gmail.com>
2021-12-15 16:58:56 -0500

    Added try-catch with alert boxes to refresh page

--- public/javas/dyRange.js
+++ public/javas/dyRange.js
@@ -15,43 +15,56 @@ const myFile = document.getElementById("myFile");
 myFile.onchange = async () => {
   var ubuffer = [];
   const fileData = myFile.files[0];
-  // file error if statesments here
-  // converts fileData to arrayBuffer, then uses an asynchronous function to turn it into a Uint8Array ubuffer
+  // file error if statesments here?
 
-  // try catch this whole block?
+  // converts fileData to arrayBuffer, then uses an asynchronous function to turn it into a Uint8Array ubuffer
   await fileData.arrayBuffer().then((buffer) => {
     ubuffer = new Uint8Array(buffer, 0, buffer.byteLength);
   });
 
   (async () => {
-    // instances ffmpeg (may take a few minutes)
-    await ffmpeg.load();
-    // writes inputted movie file to memory in a Uint8Array
-    await ffmpeg.FS("writeFile", `${fileData.name}`, ubuffer);
-    await ffmpeg.run(
-      "-i",
-      `${fileData.name}`,
-      "-vcodec",
-      "copy",
-      "-af",
-      "compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0",
-      `out_${fileData.name}`
-    );
-    // reads file to const output (still in array form), and then converted to blob
-    const output = await ffmpeg.FS("readFile", `out_${fileData.name}`);
-    const outputBlob = new Blob([output]);
+    try {
+      await ffmpeg.load();
+      // writes inputted movie file to memory in a Uint8Array
+      await ffmpeg.FS("writeFile", `${fileData.name}`, ubuffer);
+      await ffmpeg.run(
+        "-i",
+        `${fileData.name}`,
+        "-vcodec",
+        "copy",
+        "-af",
+        "compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0",
+        `out_${fileData.name}`
+      );
+    } catch (err) {
+      alert(
+        "Browser may not support SharedArrayBuffer at this time, try using chrome or firefox"
+      );
+      location.reload();
+    }
 
-    // Create object URL to get movie blob from memory, and make available for download
-    const objUrl = await URL.createObjectURL(outputBlob);
-    var a = window.document.createElement("a");
-    a.href = objUrl;
-    a.download = `out_${fileData.name}`;
+    try {
+      // reads file to const output (still in array form), and then converted to blob
+      const output = await ffmpeg.FS("readFile", `out_${fileData.name}`);
+      const outputBlob = new Blob([output]);
 
-    // Append anchor to body.
-    document.body.appendChild(a);
-    a.click();
+      // Create object URL to get movie blob from memory, and make available for download
+      const objUrl = await URL.createObjectURL(outputBlob);
+      var a = window.document.createElement("a");
+      a.href = objUrl;
+      a.download = `out_${fileData.name}`;
 
-    // Remove anchor from body
-    document.body.removeChild(a);
+      // Append anchor to body.
+      document.body.appendChild(a);
+      a.click();
+
+      // Remove anchor from body
+      document.body.removeChild(a);
+    } catch (err) {
+      alert(
+        "Problem with outputting file. Make sure file is a movie file! Page will reload upon closing this box"
+      );
+      location.reload();
+    }
   })();
 };
