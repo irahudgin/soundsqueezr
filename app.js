56e73c984455d439590d690de0e82abcdee1d120
Ira Hudgin <irahudgin@gmail.com>
2022-01-02 15:00:44 -0500

    added a test manual ssl redirect3

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ const path = require("path");
 // If more than one html page staticPath has to change
 var staticPath = path.join(__dirname, "/public");
 
-app.use((req, res, next) => {
-  res.set("Cross-Origin-Embedder-Policy", "require-corp");
-  res.set("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
-app.use(express.static(staticPath));
-app.use("/scripts", express.static(__dirname + "/node_modules/@ffmpeg/"));
-
 app.get('*', (req, res) => {
   if (req.headers['x-forwarded-proto'] != 'https') {
     res.redirect('https://www.soundsqueezr.com/' + req.url);
@@ -23,5 +15,15 @@ app.get('*', (req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(staticPath);
 });
+
+app.use((req, res, next) => {
+  res.set("Cross-Origin-Embedder-Policy", "require-corp");
+  res.set("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+app.use(express.static(staticPath));
+app.use("/scripts", express.static(__dirname + "/node_modules/@ffmpeg/"));
+
+
 
 app.listen(process.env.PORT);
