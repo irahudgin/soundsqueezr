149a32de6112eff482840d97cba75d37993f3d12
Ira Hudgin <irahudgin@gmail.com>
2022-09-12 13:00:12 -0400

    deleted old heroku server code

--- app.js
+++ app.js
@@ -16,20 +16,6 @@ app.use((req, res, next) => {
 app.use(express.static(staticPath));
 app.use("/scripts", express.static(__dirname + "/node_modules/@ffmpeg/"));
 
-// app.get('*', (req, res) => {
-//   if(req.headers["x-forwarded-proto"] === "https") {
-//     // OK, continue
-//     return next();
-//   };
-//   res.redirect('https://www.soundsqueezr.com/');
-
-//   // let protocol = req.protocol;
-
-//   // if (protocol != 'https') {
-//   //   res.redirect('https://www.soundsqueezr.com/');
-//   // }
-// });
-
 app.get("/", (req, res) => {
   res.sendFile(staticPath);
 });
